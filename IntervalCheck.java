f58ea7c48270701d94c0a68a2dc0e3adb89c6e82
ann <hedion@proton.me>
2024-06-22 22:19:00 +0200

    Initial commit

---
+++ IntervalCheck.java
@@ -0,0 +1,21 @@
+
+
+public class IntervalCheck {
+
+    public static boolean checkInt1(int a) {
+        return (a >= 49 && a <= 101);
+    }
+
+    public static boolean checkInt2(int a) {
+        return (a >= 1 && a <= 6);
+    }
+
+    public static void main(String[] args) {
+        checkInt1(10);
+        checkInt2(2);
+    }
+    
+    
+
+    
+}
