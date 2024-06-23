22e44c54388e808ba636b296b52b24e5210e3094
ann <hedion@proton.me>
2024-06-23 15:56:10 +0200

    Fahr added, methods improved

--- Converted.java
+++ Converted.java
@@ -1,16 +1,19 @@
 public class Converted {
-    public static long kiloIntoMega(long a) {
-        int b = 1024;
-        return (a / b);
+    public static int kiloIntoMega(int kilo) {
+        return (kilo / 1024);
     }
 
     public static double milesIntoKiloms(int miles) {
-        double a = 0.6214;
-        return (miles / a);
+        return (miles / 0.6214);
+    }
+
+    public static double fahrIntoCels(int fahr) {
+        return ((fahr - 32) / 1.8);
     }
 
     public static void main(String[] args) {
         System.out.println(kiloIntoMega(1000000));
-        System.out.printf("%.2f", milesIntoKiloms(15));
+        System.out.printf("%.2f\n", milesIntoKiloms(15));
+        System.out.printf("%.2f\n", fahrIntoCels(78));
     }
 }
