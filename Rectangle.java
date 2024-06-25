1b08028d1a88938b6320a97428515362ad32912d
ann <hedion@proton.me>
2024-06-25 17:15:57 +0200

    Printed result

--- Rectangle.java
+++ Rectangle.java
@@ -31,7 +31,7 @@ public class Rectangle {
 
     public static void main(String[] args) {
         Rectangle rec = new Rectangle(40, 100);
-        rec.perimRec();
+        System.out.println(rec.perimRec());
 
     }
     
