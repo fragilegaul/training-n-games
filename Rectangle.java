bb0599ac8f6243de26d5038c5902dc9a00dbdbcc
ann <hedion@proton.me>
2024-06-25 17:11:36 +0200

    Initial commit

---
+++ Rectangle.java
@@ -0,0 +1,38 @@
+
+public class Rectangle {
+
+    private int sideA;
+    private int sideB;
+
+    Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public int getSideA() {
+        return this.sideA;
+    }
+
+    public void setSideA(int sideA) {
+        this.sideA = sideA;
+    }
+
+    public int getSideB() {
+        return this.sideB;
+    }
+
+    public void setSideB(int sideB) {
+        this.sideB = sideB;
+    }
+    
+    public int perimRec() {
+        return (sideA + sideB) * 2;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle(40, 100);
+        rec.perimRec();
+
+    }
+    
+}
