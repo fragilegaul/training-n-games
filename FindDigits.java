97038df38f927564679dec0b0b202c5128022b38
ann <hedion@proton.me>
2024-06-27 22:22:24 +0200

    Sum first'n last added

--- FindDigits.java
+++ FindDigits.java
@@ -12,8 +12,15 @@ public class FindDigits {
         return a % 10;
     }
 
+    public static int findSumFirstAndLast(int a) {
+        return (findFirst(a) + findLast(a));
+    }
+
     public static void main(String[] args) {
         int num = 229345;
         System.out.println("The first digit is: " + findFirst(num) + " and the last one is: " + findLast(num));
+
+        int num1 = 45678;
+        System.out.println("The sum of the fist and last digits is: " + findSumFirstAndLast(num1));
     }
 }
