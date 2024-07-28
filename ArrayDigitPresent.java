40db411360d68793d5b36cc166c7343468e0b668
ann <hedion@proton.me>
2024-07-29 00:43:47 +0200

    Improved, every digit is checked

--- ArrayDigitPresent.java
+++ ArrayDigitPresent.java
@@ -1,16 +1,18 @@
 public class ArrayDigitPresent {
     public static int amountPresentInNegative(int[] arr, int checkDigit) {
         int count = 0;
+        String s = Integer.toString(checkDigit);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 0) {
-                int temp = Math.abs(arr[i]);
-                if (temp % 10 == checkDigit) {
+                String st = Integer.toString(arr[i]);
+                if (st.contains(s)) {
                     count++;
                 }
             }
         }
         return count;
-    }
+    }  
+    
     public static void main(String[] args) {
         int digitToCheck = 5;
         int[] array = new int[]{123, 4, -4, 5, -7, -999, -7, -11, -777, -77, -43, 45, 67, 999, 234, 173, 761};
